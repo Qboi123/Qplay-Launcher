97d428584e6beeb14aa4624321570a2bfaeaa6a3
Quinten Jungblut <30625109+Qplay123@users.noreply.github.com>
2019-05-01 19:09:32 +0200

    Update 1.4.1

--- launcher.py
+++ launcher.py
@@ -1,9 +1,6 @@
 import os
 import zipfile
-
 import wx
-
-# a.v1_3_0.__main__.Game()
 
 
 def replace2ver(string: str):
@@ -41,12 +38,14 @@ def speed():
             a = fileTotalbytes / spd
             b = time.gmtime(a)
             hours = b[3]
+            # noinspection PyShadowingBuiltins
             min = b[4]
             sec = b[5]
         except ZeroDivisionError:
             a = -1
             b = time.gmtime(a)
             hours = b[3]
+            # noinspection PyShadowingBuiltins
             min = b[4]
             sec = b[5]
         h = str(hours)
@@ -58,7 +57,7 @@ def speed():
             s = "0" + s
 
 
-def download(url, frame, panel, version):
+def download(url, panel, version):
     import urllib.request
     from threading import Thread
 
@@ -67,14 +66,13 @@ def download(url, frame, panel, version):
     global spd
     global fileTotalbytes
     global h, m, s
-    global path_and_file
     global load
     h = "23"
     m = "59"
     s = "59"
     spd = 0
     total = 0
-    print(str(url))
+
     # Get the total number of bytes of the file to download before downloading
     u = urllib.request.urlopen(str(url))
     meta = u.info()
@@ -83,27 +81,9 @@ def download(url, frame, panel, version):
     data_blocks = []
     total = 0
 
-    MAX = fileTotalbytes + 1
-
-    # Frame.SetWindowStyle(wx.MINIMIZE | wx.FRAME_NO_TASKBAR)
-
-    # Cancel = wx.Button(Frame, -1, "Cancel")
-    # Cancel.Bind(wx.EVT_BUTTON, lambda: app.Destroy())
-    #
-
-
-    # filename.destroy()
-    # file_path.destroy()
-    # Input.Destroy()
-
     frame.Show(False)
 
     load = wx.ProgressDialog("Downloading...", "Download from Qplay123/Qplay-Bubbles", maximum=100)
-
-    # load = ttk.Progressbar(root, length=750, max=MAX)
-    # load.config(value=0)
-    # load.pack()
-    # Frame.Update()
 
     active = True
 
@@ -125,7 +105,7 @@ def download(url, frame, panel, version):
             active = False
             break
 
-    data = b''.join(data_blocks)  # had to add b because I was joining bytes not strings
+    data = b''.join(data_blocks)
     u.close()
 
     if not os.path.exists("temp"):
@@ -147,19 +127,9 @@ class Launcher(wx.Panel):
         json_url = urllib.request.urlopen("https://raw.githubusercontent.com/Qplay123/Qplay-Bubbles/master/current.json")
         json_data = json_url.read().decode()
 
-        version = "v1.4.0"
-        minimum = "v1.4.0"
-        build = 10
-        min_build = 10
-
         import json
 
         self.data = json.JSONDecoder().decode(json_data)
-
-        if self.data["build"] > build:
-            outdated = True
-        else:
-            outdated = False
 
         json_url = urllib.request.urlopen("https://raw.githubusercontent.com/Qplay123/Qplay-Bubbles/master/all_versions.json")
         json_data = json_url.read().decode()
@@ -167,27 +137,23 @@ class Launcher(wx.Panel):
         import json
 
         self.all = json.JSONDecoder().decode(json_data)
-        print(self.all)
         for i in ("v1.1.0", "v1.1.1", "v1.2.0-pre1", "v1.2.0-pre2", "v1.2.0", "v1.2.1", "v1.2.2", "v1.3.0-pre1", "v1.3.0"):
             self.all.pop(i)
-
-        print(self.data)
-        print(self.all)
 
         vertical_box = wx.BoxSizer(wx.VERTICAL)
 
         self.versions = wx.Choice(self, pos=(0, 640-35), choices=list(self.all.keys()))
         self.play = wx.Button(self, label="Play!", size=wx.Size(120, 70), pos=(640-60, 640-35))
-        self.play.Bind(wx.EVT_BUTTON, self.open)
+        self.play.Bind(wx.EVT_BUTTON, lambda event: self.open())
         self.play.Show()
 
         self.SetSizer(vertical_box)
 
-    def open(self, event):
+    def open(self):
         version = self.versions.GetString(self.versions.GetSelection())
         version_dir = replace2dir(version)
         if not os.path.exists("versions/"+version_dir+"/"):
-            download("https://codeload.github.com/Qplay123/QplayBubbles-Releaes/zip/"+self.data["version"], frame, self, version)
+            download("https://github.com/Qplay123/QplayBubbles-Releaes/archive/"+self.data["version"]+".zip", self, version)
             extract_zipfile("temp/QplayBubbles-"+version+'.zip', "versions/")
             os.rename("versions/QplayBubbles-Releaes-"+version[1:], "versions/"+version_dir)
 
