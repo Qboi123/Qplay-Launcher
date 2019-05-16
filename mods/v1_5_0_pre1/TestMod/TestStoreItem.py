248d7e484cd87319a0729d9c36bbed9ad2a83c54
Quinten Jungblut <30625109+Qplay123@users.noreply.github.com>
2019-05-16 16:27:52 +0200

    Some update i don't know about.

---
+++ mods/v1_5_0_pre1/TestMod/TestStoreItem.py
@@ -0,0 +1,10 @@
+from versions.v1_5_0_pre1.base import StoreItem
+from versions.v1_5_0_pre1.fake_main import Game
+
+
+class TestStoreItem(StoreItem):
+    def __init__(self, parent: Game):
+        super().__init__(parent)
+
+    def on_buy(self, parent: Game):
+        print("TestStoreItem clicked!")
